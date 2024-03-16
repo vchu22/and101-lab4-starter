c77fe036a76a9ab47ebec4179d44a379c5a23a52
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2024-03-15 22:18:31 -0400

    finish step 1

--- app/src/main/java/com/driuft/redbutton/MainActivity.kt
+++ app/src/main/java/com/driuft/redbutton/MainActivity.kt
@@ -19,7 +19,6 @@ class MainActivity : AppCompatActivity() {
     private val redButton get() = binding.redButton
     private val blueButton get() = binding.blueButton
     private val tinyYellowButton get() = binding.tinyYellowButton
-    private val superSecretView get() = binding.SUPERSECRETPOWERLEVELVIEW
     private val sharedPref: SharedPreferences by lazy {
         getPreferences(MODE_PRIVATE)
     }
@@ -41,7 +40,6 @@ class MainActivity : AppCompatActivity() {
         blueButton.setOnClickListener { blueButtonClicked() }
         tinyYellowButton.setOnClickListener { tinyButtonClicked() }
         tinyYellowButton.setOnLongClickListener { tinyButtonLongClicked() }
-        superSecretView.setOnClickListener { unclickableButton() }
     }
 
     /**
